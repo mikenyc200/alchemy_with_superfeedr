cb2b2d58c41de52a5430e5432a1b04de4773a3a5
julien <julien.genestoux@gmail.com>
2015-04-17 22:30:34 +0200

    updated callback url

--- config/initializers/superfeedr_engine.rb
+++ config/initializers/superfeedr_engine.rb
@@ -1,6 +1,6 @@
 SuperfeedrEngine::Engine.feed_class = "Feed"
 
-SuperfeedrEngine::Engine.host = "5ea1e5ed83bf5555.a.passageway.io"
+SuperfeedrEngine::Engine.host = "cryptic-peak-7737.herokuapp.com"
 SuperfeedrEngine::Engine.login = "demo"
 SuperfeedrEngine::Engine.password = "8ac38a53cc32f71a6445e880f76fc865"
 SuperfeedrEngine::Engine.scheme = "http"
