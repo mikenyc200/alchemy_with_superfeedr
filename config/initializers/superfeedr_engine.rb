9b0c732ca51937c2089acf200b433dcd95e3320d
Ubuntu <ubuntu@ip-172-31-22-5.us-west-2.compute.internal>
2015-06-09 04:50:22 +0000

    superfeeder cred changed by mike

--- config/initializers/superfeedr_engine.rb
+++ config/initializers/superfeedr_engine.rb
@@ -2,8 +2,9 @@ SuperfeedrEngine::Engine.feed_class = "Feed"
 
 #SuperfeedrEngine::Engine.host = "cryptic-peak-7737.herokuapp.com"
 #SuperfeedrEngine::Engine.host = "677ebc2.ngrok.com"
-SuperfeedrEngine::Engine.host = "52.25.102.158:3000"
-SuperfeedrEngine::Engine.login = "Mikenyc"
+SuperfeedrEngine::Engine.host = "52.25.102.158"
+#SuperfeedrEngine::Engine.login = "Mikenyc"
+SuperfeedrEngine::Engine.login = "trackermike"
 SuperfeedrEngine::Engine.password = "YouShallNotPass"
 SuperfeedrEngine::Engine.scheme = "http"
 SuperfeedrEngine::Engine.base_path = "/secret_path_for_superfeedr/" # don't forget the trailing /
