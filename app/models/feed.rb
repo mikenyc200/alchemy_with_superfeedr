f6f7c34895463743d8c71e030664d2753974d720
Vignesh Prakasam <vigneshp.ceg+github@gmail.com>
2015-06-18 17:54:04 +0530

    discard if url already present or redirect param is present

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -16,6 +16,9 @@ class Feed < ActiveRecord::Base
       update_attributes(:status => params["status"]["http"])
 
       params['items'].each do |i|
+        url_temp =  i["permalinkUrl"]
+        #make permalink unique and discard permalinks with redirect
+        next if Entry.all.pluck(:url).include? url_temp or  url_temp.include? "redirect"
         entries.create(:atom_id => i["id"], :title => HTMLEntities.new.decode(i["title"]), :url => i["permalinkUrl"], :image => extract_image(i), :content => i["content"])
       end
     rescue => e
